eb6ae371ff937c46a61467fdb1e869c7d0b940be
Dan Miller <dan.miller3112@gmail.com>
2023-02-20 18:10:03 +0200

    init

---
+++ src/test/java/io/start/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.start.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
